e4536eedf617cb411b781ad45cffec555803876c
Alexis Josephides <ajosephides@gmail.com>
2019-09-07 11:04:40 +0100

    added some sudo code to understand flow

--- bin/pretty_cron.rb
+++ bin/pretty_cron.rb
@@ -2,6 +2,9 @@ module PrettyCron
 
   def self.run(expressionArray)
     raise 'There are the wrong number of arguments' unless expressionArray.length == 6
+    puts expressionArray
   end
+
+  run(ARGV)
 
 end
