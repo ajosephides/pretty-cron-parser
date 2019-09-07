492fbb32422ffb68285de4df017e30b78f22e87b
Alexis Josephides <ajosephides@gmail.com>
2019-09-07 12:46:41 +0100

    further unit tests to check for other types with splat expression

--- spec/unit/cron_expression_evaluator_spec.rb
+++ spec/unit/cron_expression_evaluator_spec.rb
@@ -10,9 +10,29 @@ describe CronExpressionEvaluator do
 
 
   describe '#convertToIntegersArray' do
-    it 'returns the correct range for a single "*" expression - seconds' do
+    it 'returns the correct range for a single "*" expression - minutes' do
       cron_eval = CronExpressionEvaluator.new('*', 0)
       expect(cron_eval.convert_to_integers_array).to eq((0..59).to_a)
+    end
+
+    it 'returns the correct range for a single "*" expression - hours' do
+      cron_eval = CronExpressionEvaluator.new('*', 1)
+      expect(cron_eval.convert_to_integers_array).to eq((0..23).to_a)
+    end
+
+    it 'returns the correct range for a single "*" expression - day of month' do
+      cron_eval = CronExpressionEvaluator.new('*', 2)
+      expect(cron_eval.convert_to_integers_array).to eq((1..31).to_a)
+    end
+
+    it 'returns the correct range for a single "*" expression - month' do
+      cron_eval = CronExpressionEvaluator.new('*', 3)
+      expect(cron_eval.convert_to_integers_array).to eq((1..12).to_a)
+    end
+
+    it 'returns the correct range for a single "*" expression - day of week' do
+      cron_eval = CronExpressionEvaluator.new('*', 4)
+      expect(cron_eval.convert_to_integers_array).to eq((0..6).to_a)
     end
 
   end
